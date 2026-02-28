e21bc3d9cfbe0c0cbbc5f48aa4d773b29d860d04
saurabh7414 <saurabh886020@gmail.com>
2026-02-28 10:24:52 +0530

    Time: 4 ms (71.06%) | Memory: 20.9 MB (87.67%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,13 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        d={}
-        for data in nums:
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+       d={}
+       for data in nums:
             if data in d:
                 d[data]+=1
             else:
                 d[data]=1
-        size= len(nums)
-        for key in d:
+       size= len(nums)
+       for key in d:
             count = d[key]
             if(count > (size // 2)) or count >= (size //2) and size % 2 ==0:
                 return key
