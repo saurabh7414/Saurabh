45432100a32163e738f799658e4cd0ae29445e49
saurabh7414 <saurabh886020@gmail.com>
2026-02-28 10:26:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (87.32%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mp = {')': '(', '}': '{', ']': '['}
+
+        for ch in s:
+            if ch in mp:
+                if not stack or stack.pop() != mp[ch]:
+                    return False
+            else:
+                stack.append(ch)
+
+        return not stack
